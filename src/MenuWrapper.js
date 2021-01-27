78cb41ed1b70cf7bf12b8f672aa61f64f412d487
ilaexe <ilary.poggi@yahoo.it>
2021-01-27 22:31:05 +0100

    moved category inside dish array to check constraints

--- src/MenuWrapper.js
+++ src/MenuWrapper.js
@@ -2,7 +2,10 @@ import React from "react";
 import MenuNavbar from "./MenuNavbar";
 import MenuList from "./MenuList";
 import Cart from "./Cart";
-import { ThemeConsumer } from "react-bootstrap/esm/ThemeProvider";
+import Swal from 'sweetalert2'
+
+
+
 class MenuWrapper extends React.Component {
   constructor(props) {
     super(props);
@@ -16,38 +19,86 @@ class MenuWrapper extends React.Component {
   addToCart(dish) {
     const cartItems = this.state.cartItems.slice();
     let alreadyPresent = false;
-    cartItems.forEach((it) => {
+
+    cartItems.forEach((it) => {   
+
       if (it.id === dish.id) {
-        it.count++;
-        alreadyPresent = true;
+        if(it.name == "Cheesecake"){
+          Swal.fire({
+            title:'Oops',
+            text:"There is just 1 piece of Cheesecake left"
+           })
+        }
+
+         it.count++;
+         alreadyPresent = true;
       }
     });
     if (!alreadyPresent) {
       cartItems.push({ ...dish, count: 1 });
+      console.log("Cartitems",cartItems)
     }
-    this.setState({ cartItems });
+    this.setState({ cartItems }); 
+
+    this.checkConstraint(cartItems);
+
   }
+
+
 
   removeFromCart(dish) {
     const cartItems = this.state.cartItems.slice();
 
     cartItems.forEach((it) => {
-      if (it.id == dish.id && it.count !== 0) {
+      if (it.id == dish.id) {
         it.count -= 1;
       }
-    });
-    this.setState({ cartItems });
+      if(it.count <1){
+      cartItems.splice(cartItems.indexOf(it), 1);
+      }
+    })
+    
+    this.setState({cartItems});
   }
 
   filterData(e) {
     this.setState({ selectedValue: e.currentTarget.innerHTML });
   }
 
+
+  checkConstraint(cartItems){
+
+    var result = [];
+    cartItems.reduce(function (res, value) {
+      if (!res[value.category]) {
+        res[value.category] = { category: value.category, count: 0 };
+        result.push(res[value.category]);
+      }
+      res[value.category].count += value.count;
+      return res;
+    }, {});
+
+    result.forEach(it=>{
+       if(it.count > 2){
+         Swal.fire({
+           title:'Oops',
+           text:"you cannot have more than 2 dish per category"
+          })
+        }
+    })
+
+  }
+
+
+
   render() {
     const categories = Object.keys(this.props.data);
 
-    var myarray = [];
+    let myarray = [];
     Object.entries(this.props.data).map(([category, dishes]) => {
+       dishes = dishes.map((element) => ({ ...element,
+        category: category
+      }))
       myarray.push([category, dishes]);
     });
 
