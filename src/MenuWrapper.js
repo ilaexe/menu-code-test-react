9fa10063d7c94c6589fc59413ee4534877638d6e
ilaexe <ilary.poggi@yahoo.it>
2021-01-28 13:10:25 +0100

    added more constraints

--- src/MenuWrapper.js
+++ src/MenuWrapper.js
@@ -2,9 +2,7 @@ import React from "react";
 import MenuNavbar from "./MenuNavbar";
 import MenuList from "./MenuList";
 import Cart from "./Cart";
-import Swal from 'sweetalert2'
-
-
+import Swal from "sweetalert2";
 
 class MenuWrapper extends React.Component {
   constructor(props) {
@@ -20,31 +18,18 @@ class MenuWrapper extends React.Component {
     const cartItems = this.state.cartItems.slice();
     let alreadyPresent = false;
 
-    cartItems.forEach((it) => {   
-
+    cartItems.forEach((it) => {
       if (it.id === dish.id) {
-        if(it.name == "Cheesecake"){
-          Swal.fire({
-            title:'Oops',
-            text:"There is just 1 piece of Cheesecake left"
-           })
-        }
-
-         it.count++;
-         alreadyPresent = true;
+        it.count++;
+        alreadyPresent = true;
       }
     });
     if (!alreadyPresent) {
       cartItems.push({ ...dish, count: 1 });
-      console.log("Cartitems",cartItems)
     }
-    this.setState({ cartItems }); 
-
-    this.checkConstraint(cartItems);
-
+    this.setState({ cartItems });
+    this.checkConstraint(cartItems, dish);
   }
-
-
 
   removeFromCart(dish) {
     const cartItems = this.state.cartItems.slice();
@@ -53,52 +38,78 @@ class MenuWrapper extends React.Component {
       if (it.id == dish.id) {
         it.count -= 1;
       }
-      if(it.count <1){
-      cartItems.splice(cartItems.indexOf(it), 1);
+      if (it.count < 1) {
+        cartItems.splice(cartItems.indexOf(it), 1);
       }
-    })
-    
-    this.setState({cartItems});
+    });
+
+    this.setState({ cartItems });
   }
 
   filterData(e) {
     this.setState({ selectedValue: e.currentTarget.innerHTML });
   }
 
+  checkConstraint(cartItems, dish) {
 
-  checkConstraint(cartItems){
-
-    var result = [];
+    let groupByCategory = [];
     cartItems.reduce(function (res, value) {
       if (!res[value.category]) {
         res[value.category] = { category: value.category, count: 0 };
-        result.push(res[value.category]);
+        groupByCategory.push(res[value.category]);
       }
       res[value.category].count += value.count;
       return res;
     }, {});
 
-    result.forEach(it=>{
-       if(it.count > 2){
-         Swal.fire({
-           title:'Oops',
-           text:"you cannot have more than 2 dish per category"
-          })
-        }
-    })
+    let notValid = false;
 
+    groupByCategory.forEach((it) => {
+      if (it.count > 2) {
+        Swal.fire({
+          title: "Oops",
+          text: "you cannot have more than 2 dish per category.",
+        });
+        notValid = true;
+      }
+    });
+
+    let prawn = false;
+    let salmon = false;
+    cartItems.forEach((it) => {
+      if (it.name == "Prawn cocktail") {
+        prawn = true;
+      }
+
+      if (it.name == "Salmon fillet") {
+        salmon = true;
+      }
+      if (salmon && prawn) {
+        Swal.fire({
+          title: "Oops",
+          text: "Too much fish, remove from cart Prawn cocktail or Salmon fillet.",
+        });
+      }
+      if(it.name == 'Cheesecake' && it.count > 1 ){
+        Swal.fire({
+          title: "Oops",
+          text: "There is just 1 piece of Cheesecake left.",
+        });
+        notValid = true
+      }
+    });
+
+    if (notValid == true) {
+      this.removeFromCart(dish);
+    }
   }
-
-
 
   render() {
     const categories = Object.keys(this.props.data);
 
     let myarray = [];
     Object.entries(this.props.data).map(([category, dishes]) => {
-       dishes = dishes.map((element) => ({ ...element,
-        category: category
-      }))
+      dishes = dishes.map((element) => ({ ...element, category: category }));
       myarray.push([category, dishes]);
     });
 
